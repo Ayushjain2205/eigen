c28f90d8630491f5dc9a26caa502e1c0c9dc9833
Ayush Jain <ayushjain2205@gmail.com>
2023-05-10 23:51:22 +0530

    Configure Daisy UI

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      This is dashboard
+      <button className="btn btn-primary">Test button</button>
+    </div>
+  );
+};
+
+export default page;
