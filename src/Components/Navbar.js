c24d627b99b5e5d1ab19ad56135608b8980716ad
Ayush Jain <ayushjain2205@gmail.com>
2023-05-12 01:24:45 +0530

    Add active navlink

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,6 @@
 "use client";
-
-import React, { useEffect } from "react";
+import { usePathname } from "next/navigation";
+import React, { useEffect, useState } from "react";
 import { useAccount, useConnect, useEnsName } from "wagmi";
 import { MetaMaskConnector } from "wagmi/connectors/metaMask";
 
@@ -10,16 +10,29 @@ const Navbar = () => {
     connector: new MetaMaskConnector(),
   });
 
+  const [active, setActive] = useState("");
+  const pathname = usePathname();
+
+  useEffect(() => {
+    setActive(pathname);
+  }, [pathname]);
+
   return (
-    <div className="navbar bg-base-100 h-[88px] padding-auto px-[40px]">
+    <div className="navbar bg-base-100 h-[88px] padding-auto px-[40px] border-b-2 border-[#373F41]">
       <div className="navbar-start">
         <img src="/eigen-logo.svg" className="h-[37px] w-[36px]" alt="" />
       </div>
       <div className="navbar-center">
-        <div className="flex flex-row gap-[32px]">
-          <span className="text-[20px] font-bold">EXPLORE</span>
-          <span className="text-[20px] ">MY DAOs</span>
-          <span className="text-[20px] ">MY CONTRIBUTIONS</span>
+        <div className="flex flex-row gap-[32px] text-[20px]">
+          <span
+            className={`text-${
+              active === "/dashboard" ? "gray-500 font-bold" : "base-500"
+            }`}
+          >
+            EXPLORE
+          </span>
+          <span className=" ">MY DAOs</span>
+          <span className="">MY CONTRIBUTIONS</span>
         </div>
       </div>
       <div className="navbar-end">
