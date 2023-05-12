447b16e300a2720a7a90b096bf6f2fc0ea8dee5d
Ayush Jain <ayushjain2205@gmail.com>
2023-05-12 14:57:11 +0530

    Add DAO header

---
+++ src/Components/DAOheader.js
@@ -0,0 +1,28 @@
+const DAOheader = () => {
+  return (
+    <div className="flex flex-row gap-[90px] bg-[#262626] rounded-[10px] p-[42px] h-[282px] w-[1214px]">
+      <div>
+        <img src="/dao-header.svg" alt="" />
+      </div>
+      <div>
+        <div className="flex flex-col gap-[50px]">
+          <p className="text-[#f5f5f5] text-[24px] w-[755px]">
+            VB’s DAO aims to honour the creater of Etherum by collecting images
+            of him to create a fine tuned AI model
+          </p>
+          <div className="flex flex-row gap-[10px]">
+            <button className="bg-[#FEC7C7] w-[366px] text-[24px] h-[60px] text-[#262626] rounded-[10px] px-[20px] py-[10px] mt-[20px]">
+              Make your contribution
+            </button>
+            <button className="bg-[#f5f5f5] w-[143px] text-[24px] h-[60px] text-[#262626] rounded-[10px] px-[20px] py-[10px] mt-[20px]">
+              Quit
+            </button>
+            <i class="fa-regular fa-circle-chevron-up"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DAOheader;
--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Layout from "@/Components/Layout";
 import TabsHolder from "@/Components/Tabs";
+import DAOheader from "@/Components/DAOheader";
 
 const page = () => {
   return (
     <Layout>
-      <div className="flex justify-center">
+      <div className="flex flex-col items-center mt-[40px] gap-[20px] justify-center">
+        <DAOheader />
         <TabsHolder />
       </div>
     </Layout>
