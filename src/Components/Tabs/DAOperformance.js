ea62327aa8cb7f92ade7443652efca16b07f3083
Ayush Jain <ayushjain2205@gmail.com>
2023-05-13 21:32:31 +0530

    Update DAO performance message

--- src/Components/Tabs/DAOperformance.js
+++ src/Components/Tabs/DAOperformance.js
@@ -6,6 +6,12 @@ import { data } from "../../utils/Contributions";
 const DAOperformance = () => {
   return (
     <div>
+      <div className="flex flex-row justify-between mt-[30px] mb-[30px]">
+        <span className="text-[24px]">
+          Now, you can review the contribution of your peers to fine tune the
+          inputs!
+        </span>
+      </div>
       <div className="grid grid-cols-3 gap-[42px] ">
         {data.map((item, index) => (
           <Contribution key={index} img={item.img} address={item.address} />
