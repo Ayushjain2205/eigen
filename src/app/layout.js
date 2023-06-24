8c23bf0ec36886410c63b26ef6d3892c972ba255
Ayush Jain <ayushjain2205@gmail.com>
2023-06-24 18:22:59 +0530

    change provider

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "../styles/globals.css";
 import { DM_Sans } from "next/font/google";
 import { WagmiProvider } from "@/Components/WagmiProvider";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { AuroraTestnet } from "@thirdweb-dev/chains";
 
 const inter = DM_Sans({ weight: ["500", "400", "700"], subsets: ["latin"] });
 
@@ -10,7 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html data-theme="light" lang="en">
       <title>Eigen</title>
-      <ThirdwebProvider activeChain="mumbai">
+      <ThirdwebProvider activeChain={AuroraTestnet}>
         <WagmiProvider>
           <body className={inter.className}>{children}</body>
         </WagmiProvider>
